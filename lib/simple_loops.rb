691ec434a7e29030e167bd77e8a45922ef3af3e5
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-18 17:59:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,3 +23,11 @@ def output_array(array)
     count += 1 
   end
 end
+
+def return_string_array(array)
+  count = 0 
+  while count < array.length do
+    array[count].to_s
+    count += 1 
+  end
+end
